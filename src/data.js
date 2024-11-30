c298d71ceb75de77f7c342f70072832fa7408063
sanket <jadhavsanket621@gmail.com>
2024-12-01 04:04:01 +0530

    added cosmos data

--- src/data.js
+++ src/data.js
@@ -9,6 +9,14 @@ export default [
     },
     {
         id:2,
+        title: "COSMOS",
+        description: "COSMOS showcases astronomical data using NASA's Open API",
+        techss: ["React","NASA APIs","Axios"],
+        screeshot: "cosmos.png",
+        url: "https://cosmos-sj.netlify.app/"
+    },
+    {
+        id:3,
         title: "pAInt",
         description: "React app using the OpenAI API to generate images from user prompts.",
         techss: ["React","OpenAI API",],
@@ -16,7 +24,7 @@ export default [
         url: "https://paint-reactig.netlify.app/"
     },
     {
-        id:3,
+        id:4,
         title: "Notebook",
         description: "Google Docs clone with Socket.IO for real-time collaboration.",
         techss: ["React","Socket.io","Quill Editor"],
